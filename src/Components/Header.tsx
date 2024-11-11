e4b9b2a8c36328fd37a7c825eb405162a994c543
asmajawaid <arshadasma45678@gmail.com>
2024-11-11 22:44:55 +0500

    setting header

--- src/Components/Header.tsx
+++ src/Components/Header.tsx
@@ -1,16 +1,15 @@
 import Image from 'next/image'
 import React from 'react'
 import Link from 'next/link'
-// import { FaCloudDownloadAlt } from "react-icons/fa";
 
 export default function Header() {
   return (
     <div className='bg-cyan-800 z-50 sticky top-0'>
       <header className="text-emerald-600 body-font">
-  <div className="container mx-auto flex flex-wrap p-2 flex-col md:flex-row sm:flex-col items-center">
+  <div className="container mx-auto flex flex-wrap p-2 flex-col md:flex-row md:text-sm sm:flex-col items-center">
     <Link href="/" className="flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0">
       
-      <Image src="/images/asma.png" height={50} width={50} alt="logo" className='w-10 h-10 gap-5'/>
+      <Image src="/images/asma.png" height={50} width={50} alt="logo" className='w-10 h-10'/>
      
       <span className="ml-3 text-xl font-bold">ASMA</span>
     </Link>
@@ -25,7 +24,6 @@ export default function Header() {
      <Link href="">
     <button className="font-semibold inline-flex items-center bg-gray-100 border-0 py-1 px-3 focus:outline-none hover:bg-cyan-800 hover:text-gray-100 rounded text-cyan-800 mt-4 md:mt-0">
       Download CV
-      {/* <FaCloudDownloadAlt className='m-2 '/> */}
     </button> </Link>
   </div>
 </header>
